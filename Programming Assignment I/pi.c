3a805cb759380aa43ced9ae154cc3c5abb26ec29
zhongturtle <zhongturtle@gmail.com>
2019-11-03 17:22:55 +0800

    hw1 finish

--- Programming Assignment I/pi.c
+++ Programming Assignment I/pi.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-unsigned long long  number_of_cpu, number_of_tosses, number_in_circle, toss, each_thread_toss;
+unsigned long long  number_in_circle = 0, toss, each_thread_toss;
 //pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex;
 void* dart_toss(){
     unsigned long long number_in_circle_each_pthread = 0;
+    unsigned int seed = time(NULL);
     double distance_squared, x, y;
-    for (toss = 0; toss < number_of_tosses; toss++) {
+    unsigned long long toss_pthred;
+    for (toss_pthred = 0; toss_pthred < each_thread_toss; toss_pthred++) {
         // x = random double between -1 and 1;
-        x = 2 * (double) rand() / RAND_MAX - 1.0;
+        x = 2 * (double) rand_r(&seed) / RAND_MAX - 1.0;
         // y = random double between -1 and 1;
-        y = 2 * (double) rand() / RAND_MAX - 1.0;
+        y = 2 * (double) rand_r(&seed) / RAND_MAX - 1.0;
         distance_squared = x*x + y*y;
 
         if (distance_squared <= 1)
@@ -27,8 +29,9 @@ void* dart_toss(){
 
 int main(int argc, char **argv)
 {
-    double pi_estimate, distance_squared, x, y;
+    long double pi_estimate, distance_squared, x, y;
     //unsigned long long  number_of_cpu, number_of_tosses, number_in_circle, toss;
+    unsigned long long  number_of_cpu, number_of_tosses;
     if ( argc < 2) {
         exit(-1);
     }
@@ -37,20 +40,21 @@ int main(int argc, char **argv)
     if (( number_of_cpu < 1) || ( number_of_tosses < 0)) {
         exit(-1);
     }
-    pthread_attr_t attr;
+    //pthread_attr_t attr;
 	pthread_t* threads = malloc(number_of_cpu * sizeof(pthread_t));
-    number_in_circle = 0;
+    // number_in_circle = 0;
     each_thread_toss = number_of_tosses / number_of_cpu;
+    // printf("each_thread_toss : %llu\n", each_thread_toss);
     
-    pthread_attr_init(&attr);
+    //pthread_attr_init(&attr);
 	pthread_mutex_init(&mutex, NULL);
 
     // Creat pthreads
-    for(int i = 0; i < number_of_cpu; i++){
+    for(unsigned long long i = 0; i < number_of_cpu; i++){
         pthread_create(&threads[i], NULL, dart_toss, NULL);
     }
 
-    for(int i = 0; i < number_of_cpu; i++){
+    for(unsigned long long i = 0; i < number_of_cpu; i++){
         pthread_join(threads[i], NULL);
     }
     
@@ -66,8 +70,8 @@ int main(int argc, char **argv)
         if (distance_squared <= 1)
             number_in_circle++;
     }*/
-    pi_estimate = 4*number_in_circle/((double) number_of_tosses);
+    pi_estimate = 4*number_in_circle/((long double) number_of_tosses);
 
-    printf("%f\n",pi_estimate);
+    printf("%LF\n",pi_estimate);
     return 0;
 }
